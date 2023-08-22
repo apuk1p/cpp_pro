4160bc66467ca9313d2dd223dc52c3fb0bd7f30a
apuk1p <apuk1p@mail.ru>
2023-08-22 20:18:12 +0300

    changes

--- ip_filter.cpp
+++ ip_filter.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <algorithm>
 
 // ("",  '.') -> [""]
 // ("11", '.') -> ["11"]
@@ -10,6 +12,13 @@
 // ("11.", '.') -> ["11", ""]
 // (".11", '.') -> ["", "11"]
 // ("11.22", '.') -> ["11", "22"]
+
+void circle(const std::vector<std::string>& val);
+void printVec(std::vector<std::vector<std::string>>& pool);
+void printVec(std::vector<std::vector<std::string>>& pool, int index, int value);
+void printVec(std::vector<std::vector<std::string>>& pool, int index1, int index2, int value1, int value2);
+void printAnyVec(std::vector<std::vector<std::string>>& pool, int value);
+
 std::vector<std::string> split(const std::string &str, char d)
 {
 	std::vector<std::string> r;
@@ -33,91 +42,35 @@ int main(int argc, char const *argv[])
 {
 	try
 	{
+		std::ifstream input("D:/cpp_works/homework02/ip_filters.txt", std::ios_base::in);
 		std::vector<std::vector<std::string> > ip_pool;
-
-		for(std::string line; std::getline(std::cin, line);)
+		ip_pool.reserve(1000);
+		
+		for(std::string line; std::getline(input, line);)
 		{
 			std::vector<std::string> v = split(line, '\t');
 			ip_pool.push_back(split(v.at(0), '.'));
 		}
 
-		// TODO reverse lexicographically sort
-		
-		for(const auto& val : ip_pool)
-		{
-			for(const auto& valip : val)
-			{
-				if(valip != val)
-				{
-					std::cout << ".";
-				}
-				std::cout << valip;
-			}
-			std::cout << std::endl;
-		}
 
-		// 222.173.235.246
-		// 222.130.177.64
-		// 222.82.198.61
-		// ...
-		// 1.70.44.170
-		// 1.29.168.152
-		// 1.1.234.8
+		std::sort( ip_pool.begin(), ip_pool.end(), [](const std::vector<std::string>& i, const std::vector<std::string>& b)
+					{
+						for(auto it = i.cbegin(), it2 = b.cbegin(); it != i.cend() && it2 != b.cend(); ++it, ++it2)
+						{
+							auto comp1 = std::stoi(*it);
+							auto comp2 = std::stoi(*it2);
+							if (comp1 == comp2 ) { continue; }
+							return comp1 > comp2;
+						}
+						return false;
+					} );
 
-		// TODO filter by first byte and output
-		// ip = filter(1)
 
-		// 1.231.69.33
-		// 1.87.203.225
-		// 1.70.44.170
-		// 1.29.168.152
-		// 1.1.234.8
+		printVec(ip_pool);
+		printVec(ip_pool, 0, 1);
+		printVec(ip_pool, 0, 1, 46, 70);
+		printAnyVec(ip_pool, 46);
 
-		// TODO filter by first and second bytes and output
-		// ip = filter(46, 70)
-
-		// 46.70.225.39
-		// 46.70.147.26
-		// 46.70.113.73
-		// 46.70.29.76
-
-		// TODO filter by any byte and output
-		// ip = filter_any(46)
-
-		// 186.204.34.46
-		// 186.46.222.194
-		// 185.46.87.231
-		// 185.46.86.132
-		// 185.46.86.131
-		// 185.46.86.131
-		// 185.46.86.22
-		// 185.46.85.204
-		// 185.46.85.78
-		// 68.46.218.208
-		// 46.251.197.23
-		// 46.223.254.56
-		// 46.223.254.56
-		// 46.182.19.219
-		// 46.161.63.66
-		// 46.161.61.51
-		// 46.161.60.92
-		// 46.161.60.35
-		// 46.161.58.202
-		// 46.161.56.241
-		// 46.161.56.203
-		// 46.161.56.174
-		// 46.161.56.106
-		// 46.161.56.106
-		// 46.101.163.119
-		// 46.101.127.145
-		// 46.70.225.39
-		// 46.70.147.26
-		// 46.70.113.73
-		// 46.70.29.76
-		// 46.55.46.98
-		// 46.49.43.85
-		// 39.46.86.85
-		// 5.189.203.46
 	}
 	catch(const std::exception &e)
 	{
@@ -125,4 +78,60 @@ int main(int argc, char const *argv[])
 	}
 
 	return 0;
+}
+
+
+void circle(const std::vector<std::string>& val)
+{
+	for(const auto& val1 :val)
+	{
+		if (&val1 != &val.at(0))
+		{
+			std::cout << ".";
+
+		}
+		std::cout << val1;
+	}
+	std::cout << std::endl;
+}
+
+void printVec(std::vector<std::vector<std::string>>& pool)
+{
+	for(const auto& val : pool)
+	{
+		circle(val);
+	}
+}
+
+void printVec(std::vector<std::vector<std::string>>& pool, int index, int value)
+{
+	for(const auto& val : pool)
+	{
+		if(std::stoi(val.at(index)) == value)
+		{
+			circle(val);
+		}
+	}
+}
+
+void printVec(std::vector<std::vector<std::string>>& pool, int index1, int index2, int value1, int value2)
+{
+	for(const auto& val : pool)
+	{
+		if(std::stoi(val.at(index1)) == value1 && std::stoi(val.at(index2)) == value2)
+		{
+			circle(val);
+		}
+	}
+}
+
+void printAnyVec(std::vector<std::vector<std::string>>& pool, int value)
+{
+	for(const auto& val : pool)
+	{
+		if( std::any_of(val.cbegin(), val.cend(), [&value](const std::string& i){return std::stoi(i) == value;}) )
+		{
+			circle(val);
+		}
+	}
 }
